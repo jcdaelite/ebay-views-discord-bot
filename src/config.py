35fe8e86ba69a97c272e88f43fab38f195a804f6
jcdaelite <106854280+jcdaelite@users.noreply.github.com>
2022-06-03 22:50:30 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 # Discord application token
-TOKEN = '' 
+TOKEN = 'OTgyNDc0MjU3Mjk0NTY1NDE3.GHcY55.lBQ1MPuX9t12-gVlwaKGHd6J0wi_54Iwt5HLKg' 
 
 # You can change the max amount of views so people don't add stupidly large amounts of views
 MAX_VIEWS = 50
